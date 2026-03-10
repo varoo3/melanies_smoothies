198417c1cb96357ed43695747985ff9ae0950b83
varoo3 <38317891+varoo3@users.noreply.github.com>
2026-03-10 22:23:40 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -24,6 +24,8 @@ st.dataframe(data=my_dataframe, use_container_width=True)
 
 #Convert the Snowpark Dataframe to a Pandas Dataframe so we can use the LOC function
 pd_df=my_dataframe.to_pandas()
+st.dataframe(pd_df)
+st.stop()
 
 
 ingredients_list = st.multiselect(
